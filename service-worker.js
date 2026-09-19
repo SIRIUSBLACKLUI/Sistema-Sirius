3f1766c38bbbb844271d8455f462ebeb903fc3d9
SIRIUSBLACKLUI <alyssonluiz6167@gmail.com>
2026-09-19 02:43:45 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,15 @@
-/* SIRIUS v2.22.76 // self-destruct service worker. */
+/* SIRIUS v2.22.78 // REMOVE LEGACY SERVICE WORKER AND CACHES */
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch(_){ }try{await self.registration.unregister();}catch(_){ }try{const cs=await self.clients.matchAll({type:'window',includeUncontrolled:true});await Promise.all(cs.map(c=>c.navigate(c.url)));}catch(_){ }})());});
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    try{
+      const keys=await caches.keys();
+      await Promise.all(keys.map(k=>caches.delete(k)));
+    }catch(_){ }
+    try{await self.registration.unregister();}catch(_){ }
+    try{
+      const clientsList=await self.clients.matchAll({type:'window',includeUncontrolled:true});
+      await Promise.all(clientsList.map(c=>c.navigate(c.url)));
+    }catch(_){ }
+  })());
+});
