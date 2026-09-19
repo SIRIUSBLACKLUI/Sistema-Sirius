a0298313b9c791560a78b2d71c0eba77b22c5108
SIRIUSBLACKLUI <alyssonluiz6167@gmail.com>
2026-09-19 02:16:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,3 @@
-/* SIRIUS v2.22.74 // self-destruct service worker.
-   If an older SIRIUS release registered /sw.js, this update removes it and its caches. */
+/* SIRIUS v2.22.75 // self-destruct service worker. */
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>{
-  event.waitUntil((async()=>{
-    try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch(_){ }
-    try{await self.registration.unregister();}catch(_){ }
-    try{
-      const clientsList=await self.clients.matchAll({type:'window',includeUncontrolled:true});
-      await Promise.all(clientsList.map(c=>c.navigate(c.url)));
-    }catch(_){ }
-  })());
-});
+self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch(_){ }try{await self.registration.unregister();}catch(_){ }try{const cs=await self.clients.matchAll({type:'window',includeUncontrolled:true});await Promise.all(cs.map(c=>c.navigate(c.url)));}catch(_){ }})());});
