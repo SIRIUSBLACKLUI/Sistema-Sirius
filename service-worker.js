ae9783ae4064762c1e2be62db26965b53c7a0e9e
SIRIUSBLACKLUI <alyssonluiz6167@gmail.com>
2026-09-19 02:27:31 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-/* SIRIUS v2.22.75 // self-destruct service worker. */
+/* SIRIUS v2.22.76 // self-destruct service worker. */
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch(_){ }try{await self.registration.unregister();}catch(_){ }try{const cs=await self.clients.matchAll({type:'window',includeUncontrolled:true});await Promise.all(cs.map(c=>c.navigate(c.url)));}catch(_){ }})());});
